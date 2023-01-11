34a8f3a459c11d48875a7ea9bcaeaafd0f95f727
b1xcy <b1xcy@foxmail.com>
2023-01-11 14:28:13 +0800

    完成下载功能

--- download.php
+++ download.php
@@ -9,7 +9,7 @@ if (!isset($_POST['filename']) || !isset($_POST['name'])) {
 }
 $filename=$_POST['filename'];
 $name=$_POST['name'];
-$dir='./upload'.$name.$filename;
+$dir='./upload/'.$name.'/'.$filename;
 if (!isset($_SESSION['islogin']) || $_SESSION['islogin']!==1) {
     echo ("<script>alert('未登陆，即将跳转到登陆界面');</script>");
     echo '<script>window.location.href="login.php"</script>';
@@ -20,5 +20,12 @@ if ($name!=$_SESSION['name']) {
     echo ('<script>window.location.href="index.php"</script>');
     die();
 }
-echo$dir;
+$file=fopen($dir,"rb");
+Header ( "Content-type: application/octet-stream" );
+Header ( "Accept-Ranges: bytes" );
+Header ( "Accept-Length: " . filesize ( $dir ) );
+Header ( "Content-Disposition: attachment; filename=" . $filename );
+echo fread ( $file, filesize ( $dir ) );    
+fclose ( $file );    
+exit ();
 ?>
